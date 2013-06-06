ccbfd8d99377d9d080915bb9f7aa48dd8a2912ee
HomuHomu <ecoco666@gmail.com>
2013-06-06 23:11:43 +0900

    Remove:overlay:TelephonyProvider of ATT

--- device.mk
+++ device.mk
@@ -17,8 +17,6 @@
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jfltedcm/jfltedcm-vendor.mk)
 
-## device overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltedcm/overlay
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay-gsm
 
