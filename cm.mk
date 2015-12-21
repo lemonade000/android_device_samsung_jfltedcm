1f3c8cf203bf92d7ac0c721d9775185630415f75
sakuramilk <c.sakuramilk@gmail.com>
2015-12-21 20:19:23 +0900

    update fingerprint
    Change-Id: Iad73d045440d49f035b7dfc9f4f347c499583eb0

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltedcm \
   TARGET_DEVICE=jfltedcm \
-  BUILD_FINGERPRINT="samsung/SC-04E/SC-04E:4.4.2/KOT49H/SC04EOMUFNF6:user/release-keys" \
-  PRIVATE_BUILD_DESC="jfltedcm-user 4.4.2 KOT49H SC04EOMUFNF6 release-keys"
+  BUILD_FINGERPRINT="samsung/SC-04E/SC-04E:5.0.1/LRX22C/SC04EOMUGOI4:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltedcm-user 5.0.1 LRX22C SC04EOMUGOI4 release-keys"
 
 PRODUCT_NAME := cm_jfltedcm
 PRODUCT_DEVICE := jfltedcm
