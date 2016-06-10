64f0f1268ee1b897b115aea71fafc6896b21d1c8
ma34s <ma34s.sss@gmail.com>
2016-06-11 01:30:47 +0900

    change TARGET_DEVICE jfltedcm
    Change-Id: Ib3ad85220d8fe63dc2f0dc3d6e939352bd1249d6

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=SC-04E \
-  TARGET_DEVICE=SC-04E \
+  TARGET_DEVICE=jfltedcm \
   BUILD_FINGERPRINT="samsung/SC-04E/SC-04E:5.0.1/LRX22C/SC04EOMUGOI4:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltedcm-user 5.0.1 LRX22C SC04EOMUGOI4 release-keys"
 
