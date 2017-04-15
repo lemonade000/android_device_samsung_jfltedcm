838f83dd970fc55864b73d11d73262dc045ebe14
sakuraba <sakura.bar.com@gmail.com>
2017-04-15 23:30:12 +0900

    Rebrand to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/jfltedcm/full_jfltedcm.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -9,5 +9,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/SC-04E/SC-04E:5.0.1/LRX22C/SC04EOMUGOI4:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltedcm-user 5.0.1 LRX22C SC04EOMUGOI4 release-keys"
 
-PRODUCT_NAME := cm_jfltedcm
+PRODUCT_NAME := lineage_jfltedcm
 PRODUCT_DEVICE := jfltedcm
